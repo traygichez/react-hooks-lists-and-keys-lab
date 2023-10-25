e00a32f98624bda79df79eabf88bc52fea3a2d9c
tray <onesimus.githua@student.moringaschool.com>
2023-10-25 23:00:09 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import links from "./Links";
+
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  return <nav>{links.map((link,index)=>(
+  <a key={link} href={`#${link}`}>
+    {link}
+  </a>
+  ))}</nav>;
+};
 
 export default NavBar;
